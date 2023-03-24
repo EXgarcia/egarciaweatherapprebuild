d2fe9a98106847cb987ec50e5e4574265615ce35
Elizar Garcia <garciaelizar@yahoo.com>
2023-03-24 15:46:02 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//Fernando Aguilar: upon button click the cards the weather for the day and the five day weathercast. i can see that css and styling was under development, so good job on getting started on that. Would have like to see a search bar so i could look up diffetent cities. overall good attemp on the weather app. i cant tell you learned more from the last time.
+
+
 import React from 'react';
 import './App.css';
 import GetWeather from './components/homepage/GetWeather';
